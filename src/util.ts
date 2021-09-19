eb83fa0c6b1a776a7dfdccba02a0ffe2b5cfa8c8
VikramK <Vikramk.0901@gmail.com>
2021-09-19 10:26:40 -0400

    Add typescript, eslint, prettier etc

--- src/util.ts
+++ src/util.ts
@@ -6,9 +6,9 @@ export function isVoid(val) {
 export function readValue(o, s) {
   s = s.replace(/\[(\w+)\]/g, '.$1'); // convert indexes to properties
   s = s.replace(/^\./, '');           // strip a leading dot
-  var a = s.split('.');
-  for (var i = 0, n = a.length; i < n; ++i) {
-      var k = a[i];
+  const a = s.split('.');
+  for (let i = 0, n = a.length; i < n; ++i) {
+      const k = a[i];
       if (k in o) {
           o = o[k];
       } else {
