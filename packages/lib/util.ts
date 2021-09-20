69dc92045d07f67473400734145123542fbc4885
VikramK <Vikramk.0901@gmail.com>
2021-09-20 10:54:32 -0400

    Add storybook

--- packages/lib/util.ts
+++ packages/lib/util.ts
@@ -5,15 +5,15 @@ export function isVoid(val) {
 // https://stackoverflow.com/questions/6491463/accessing-nested-javascript-objects-and-arrays-by-string-path
 export function readValue(o, s) {
   s = s.replace(/\[(\w+)\]/g, '.$1'); // convert indexes to properties
-  s = s.replace(/^\./, '');           // strip a leading dot
+  s = s.replace(/^\./, ''); // strip a leading dot
   const a = s.split('.');
   for (let i = 0, n = a.length; i < n; ++i) {
-      const k = a[i];
-      if (k in o) {
-          o = o[k];
-      } else {
-          return;
-      }
+    const k = a[i];
+    if (k in o) {
+      o = o[k];
+    } else {
+      return;
+    }
   }
   return o;
 }
