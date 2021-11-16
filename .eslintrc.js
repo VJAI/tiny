f59da70b488745db0b20e151f12c28fa5cc0debf
Vijaya Anand <vijay.prideparrot@gmail.com>
2021-11-17 01:38:04 +0530

    config changes

--- .eslintrc.js
+++ .eslintrc.js
@@ -50,7 +50,6 @@ module.exports = {
         }
       }
     ],
-    '@typescript-eslint/no-shadow': ['warn'],
     'arrow-parens': ['error', 'as-needed'],
     'node/no-deprecated-api': ['warn']
   },
@@ -62,7 +61,6 @@ module.exports = {
         '@typescript-eslint/ban-ts-ignore': 'off',
         '@typescript-eslint/no-empty-function': 'off',
         '@typescript-eslint/no-explicit-any': 'off',
-        '@typescript-eslint/no-floating-promises': 1,
         '@typescript-eslint/no-unused-vars': 'off',
         '@typescript-eslint/no-var-requires': 'off',
         '@typescript-eslint/no-shadow': ['off'],
